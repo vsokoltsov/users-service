017e3210b8348d2a0069e9cf0329d4252a3f8fee
Vadim Sokoltsov <vforvad@gmail.com>
2020-03-15 22:19:45 +0300

    Add logging middleware

--- main.go
+++ main.go
@@ -8,9 +8,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
 
+func main() {
 	router := api.InitRouter(mux.NewRouter())
+	router.Use(loggingMiddleware)
 	log.Println("Starting server on port 8000")
 	err := http.ListenAndServe(":8000", router)
 	if err != nil {
