deb4cc01e8a26fab178f80cba46f6eda7920c0fc
Vadim Sokoltsov <vforvad@gmail.com>
2020-04-11 00:00:20 +0300

    Add datetime fields to user model

--- app/models/user.go
+++ app/models/user.go
@@ -1,9 +1,13 @@
 package models
 
+import "time"
+
 // User defines a user representation
 type User struct {
-	ID        int    `db:"id"`
-	FirstName string `db:"first_name"`
-	LastName  string `db:"last_name"`
-	Email     string `db:"email"`
+	ID         int        `db:"id"`
+	FirstName  string     `db:"first_name"`
+	LastName   string     `db:"last_name"`
+	Email      string     `db:"email"`
+	CreatedAt  time.Timer `db:"created_at"`
+	UpdateddAt time.Timer `db:"updated_at"`
 }
