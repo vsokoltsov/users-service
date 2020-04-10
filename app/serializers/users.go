209f90c15e7120a4d37cf8b05a16a39f1e5c77a2
Vadim Sokoltsov <vforvad@gmail.com>
2020-04-10 23:00:07 +0300

    Update serializer

--- app/serializers/users.go
+++ app/serializers/users.go
@@ -5,6 +5,7 @@ type UserSerializer struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
+	ID        int    `json:"id"`
 }
 
 // GetUserSerializer generates serializer for user model
