3e686b4d5bdba889acb9f3b2e63c9fff42f43173
Vadim Sokoltsov <vforvad@gmail.com>
2020-04-10 23:00:34 +0300

    Add user's retrieving from the database

--- app/api/v1/users.go
+++ app/api/v1/users.go
@@ -2,12 +2,25 @@ package v1
 
 import (
 	"encoding/json"
-	"github.com/vsokoltsov/users-service/app/serializers"
 	"net/http"
+
+	"github.com/vsokoltsov/users-service/app/models"
+	"github.com/vsokoltsov/users-service/app/serializers"
+	"github.com/vsokoltsov/users-service/app/utils"
 )
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	user := serializers.GetUserSerializer()
-	json.NewEncoder(w).Encode(user)
+	var serializedUsers = []serializers.UserSerializer{}
+	var users = []models.User{}
+	utils.DB.Select(&users, "select * from users")
+	for _, user := range users {
+		serializedUsers = append(serializedUsers, serializers.UserSerializer{
+			ID:        user.ID,
+			FirstName: user.FirstName,
+			LastName:  user.LastName,
+			Email:     user.Email,
+		})
+	}
+	json.NewEncoder(w).Encode(serializedUsers)
 }
