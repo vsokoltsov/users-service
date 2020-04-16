8412a7cacedb7975dce46ef2691e1ed54ec8b659
Vadim Sokoltsov <vforvad@gmail.com>
2020-04-17 00:14:45 +0300

    Update user model and password hashing

--- app/models/user.go
+++ app/models/user.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User defines a user representation
@@ -9,7 +11,17 @@ type User struct {
 	ID        int       `db:"id"`
 	FirstName string    `db:"first_name"`
 	LastName  string    `db:"last_name"`
+	Password  string    `db:"password"`
 	Email     string    `db:"email"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
+}
+
+// SetPassword hash given password and save it to the user
+func (u *User) SetPassword(password string) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	u.Password = string(hashedPassword)
 }
